cf03704a3f814de11306fa8f2b7be0308a959792
Zaur0602 <ikhrrk6776@GMAIL.COM>
2022-02-10 14:15:17 +0300

    first comit

---
+++ Linq_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             int[] intArray = new int[] { 1, 2, 3 };
+            IEnumerable<int> ints = intArray.Select(i => i);
+
+            foreach (int i in ints)
+                Console.WriteLine(i);
+
+            // Изменить элемент, в источнике данных
+            intArray[0] = 5;
+
+            Console.WriteLine("---------");
+
+            foreach (int i in ints)
+                Console.WriteLine(i);
+        
+        }
+    }
+}
